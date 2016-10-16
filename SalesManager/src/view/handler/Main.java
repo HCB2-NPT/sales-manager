f4e8aa97b8a92ddebec4dc28ef7177325ddb81ec
phanhaugiang <thienthientan.tin@gmail.com>
2016-10-16 18:25:47 +0700

    update UI

--- SalesManager/src/view/handler/Main.java
+++ SalesManager/src/view/handler/Main.java
@@ -7,6 +7,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -21,7 +23,10 @@ public class Main {
     private JFXButton btn_Sell;
     
     @FXML
-    private BorderPane bp_xuly;
+    private JFXTabPane tab_pane;
+    
+    @FXML
+    private JFXTabPane tab_process;
     
     static Stage _currentStage = null;
     public static void callMain(){
@@ -57,16 +62,22 @@ public class Main {
     
     @FXML
     void Buy_Click() throws IOException {
+    	
     	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../BuyItem.fxml"));
-		Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
-    	bp_xuly.getChildren().setAll(root);
+		Scene sc = new Scene(root);
+		Tab t = new Tab("Buy Item",root);
+		tab_process.setTabClosingPolicy(TabClosingPolicy.SELECTED_TAB);
+		
+		tab_process.getTabs().add(t);
+		t.setClosable(true);
+		
     }
     
     @FXML
     void Sell_Click() throws IOException {
-    	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../SellItem.fxml"));
-		Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
-    	bp_xuly.getChildren().setAll(root);
+    	//BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../SellItem.fxml"));
+		//Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
+    	//bp_xuly.getChildren().setAll(root);
     }
     
     @FXML
@@ -78,15 +89,16 @@ public class Main {
     
     @FXML
     void Bill_Click() throws IOException {
-    	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../Bills.fxml"));
-		Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
-    	bp_xuly.getChildren().setAll(root);
+    	//BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../Bills.fxml"));
+		//Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
+    	//bp_xuly.getChildren().setAll(root);
     }
     
     @FXML
     void Payment_Click() throws IOException {
-    	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../Payment.fxml"));
-		Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
-    	bp_xuly.getChildren().setAll(root);
+    	//BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../Payment.fxml"));
+		//Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
+    	//bp_xuly.getChildren().setAll(root);
     }
+    
 }
