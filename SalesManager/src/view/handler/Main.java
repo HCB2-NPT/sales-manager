46a827fafabbefe4747d0a781f41814914e1757b
phanhaugiang <thienthientan.tin@gmail.com>
2016-10-16 14:17:32 +0700

    Update UI

--- SalesManager/src/view/handler/Main.java
+++ SalesManager/src/view/handler/Main.java
@@ -14,15 +14,52 @@ public class Main {
     private JFXButton btn_Buy;
     
     @FXML
+    private JFXButton btn_Sell;
+    
+    @FXML
     private BorderPane bp_xuly;
+    
+    @FXML
+    private JFXButton btn_AddItems;
+    
+    @FXML
+    private JFXButton btn_Bill;
+    
+    @FXML
+    private JFXButton btn_Payment;
     
     @FXML
     void Buy_Click() throws IOException {
     	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../BuyItem.fxml"));
 		Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
     	bp_xuly.getChildren().setAll(root);
-		
-		
-		
+    }
+    
+    @FXML
+    void Sell_Click() throws IOException {
+    	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../SellItem.fxml"));
+		Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
+    	bp_xuly.getChildren().setAll(root);
+    }
+    
+    @FXML
+    void AddItems_Click() throws IOException {
+    	//BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../.fxml"));
+		//Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
+    	//bp_xuly.getChildren().setAll(root);
+    }
+    
+    @FXML
+    void Bill_Click() throws IOException {
+    	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../Bills.fxml"));
+		Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
+    	bp_xuly.getChildren().setAll(root);
+    }
+    
+    @FXML
+    void Payment_Click() throws IOException {
+    	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource( "../Payment.fxml"));
+		Scene sc = new Scene(root,bp_xuly.getWidth(),bp_xuly.getHeight());
+    	bp_xuly.getChildren().setAll(root);
     }
 }
