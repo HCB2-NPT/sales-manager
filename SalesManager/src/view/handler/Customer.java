2275f31d5ddf57a39314e7fda78774edbccd7946
phanhaugiang <thienthientan.tin@gmail.com>
2016-10-24 16:04:52 +0700

    doing Customer

--- SalesManager/src/view/handler/Customer.java
+++ SalesManager/src/view/handler/Customer.java
@@ -2,10 +2,17 @@ package view.handler;
 
 import com.jfoenix.controls.JFXTextField;
 
+import dao.hibernate_adapters.CustomerAdapter;
+import helper.MessageBox;
+import helper.ObservableListConverter;
+import helper.TableViewHelper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.KeyEvent;
+import pojo.*;
 
 public class Customer {
 	@FXML
@@ -24,38 +31,72 @@ public class Customer {
     private JFXTextField txt_namecustomerSearch;
 
     @FXML
-    private TableView<Customer> tb_listcustomer;
+    private TableView<pojo.Customer> tb_listcustomer;
 
     @FXML
-    private TableColumn<Customer, String> tb_cName;
+    private TableColumn<pojo.Customer, String> tb_cName;
 
     @FXML
-    private TableColumn<Customer, String> tb_cpersonalid;
+    private TableColumn<pojo.Customer, String> tb_cpersonalid;
 
     @FXML
-    private TableColumn<Customer, String> tb_cPhone;
+    private TableColumn<pojo.Customer, String> tb_cPhone;
 
     @FXML
-    private TableColumn<Customer, String> tb_cCompany;
+    private TableColumn<pojo.Customer, String> tb_cCompany;
 
     @FXML
     void Add_Click() {
-
+    	if (CustomerAdapter.get(txt_personalid.getText())!=null) {
+			MessageBox.Show("Personal ID had exsisted", "Alert");
+		}
+    	if (txt_name!= null && !txt_name.getText().equals("") ) {
+			if (txt_personalid!=null && !txt_personalid.getText().equals("")) {
+				pojo.Customer c = new pojo.Customer();
+				c.setName(txt_name.getText());
+				c.setPersonalId(txt_personalid.getText());
+				c.setCompany(txt_company.getText());
+				c.setPhoneNumber(txt_phone.getText());
+				c.setCreated(true);
+				tb_listcustomer.getItems().add(c);
+			}
+			else {
+				MessageBox.Show("PersonalID cannot Empty", "Alert");
+			}
+			
+		}else {
+			MessageBox.Show("Name cannot Empty", "Alert");
+		}
     }
 
     @FXML
     void Refresh_Click() {
-
+    	tb_listcustomer.setItems(ObservableListConverter.L2OL(CustomerAdapter.getAll()));
     }
 
     @FXML
     void Search_Click() {
-
+    	if (!txt_namecustomerSearch.getText().equals("")) {
+    		String s = txt_namecustomerSearch.getText().toLowerCase();
+        	ObservableList<pojo.Customer> list = FXCollections.observableArrayList();
+        	for (pojo.Customer i : tb_listcustomer.getItems()) {
+    			if (i.getName().toLowerCase().contains(s)) {
+    				list.add(i);
+    			}
+    		}
+        	if (list.size()<=0) {
+        		MessageBox.Show("Can't find Customer have name ''" + s +"''!", "Alert");
+        		return;
+    		}
+        	tb_listcustomer.getItems().clear();
+        	tb_listcustomer.setItems(list);	
+		}
+    	
     }
 
     @FXML
     void save() {
-
+    	
     }
 
     @FXML
@@ -71,5 +112,11 @@ public class Customer {
         assert tb_cPhone != null : "fx:id=\"tb_cPhone\" was not injected: check your FXML file 'Customer.fxml'.";
         assert tb_cCompany != null : "fx:id=\"tb_cCompany\" was not injected: check your FXML file 'Customer.fxml'.";
 
+        tb_cName.setCellValueFactory(TableViewHelper.getPropertyValueFactory("name"));
+		tb_cpersonalid.setCellValueFactory(TableViewHelper.getPropertyValueFactory("personalId"));
+		tb_cPhone.setCellValueFactory(TableViewHelper.getPropertyValueFactory("phoneNumber"));
+		tb_cCompany.setCellValueFactory(TableViewHelper.getPropertyValueFactory("company"));
+		
+		tb_listcustomer.setItems(ObservableListConverter.L2OL(CustomerAdapter.getAll()));
     }
 }
